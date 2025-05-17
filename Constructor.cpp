80067ccc19f79b99140fd3b016c399fde7cdd64a
restuchan <restu.chan188@gmail.com>
2025-05-17 14:55:06 +0700

    menambahkan acces modifier pada class buku

--- Constructor.cpp
+++ Constructor.cpp
@@ -12,4 +12,20 @@ private:
     string penulis;
     bool dipinjam;
 
+public:
+    Buku(string j, string p) : judul(j), penulis(p), dipinjam(false) {}
+
+    void tampilkanInfo() {
+        cout << "Judul: " << judul << ", Penulis: " << penulis;
+        cout << ", Status: " << (dipinjam ? "Dipinjam" : "Tersedia") << endl;
+    }
+
+    bool isDipinjam() const { return dipinjam; }
+    void pinjam() { dipinjam = true; }
+    void kembalikan() { dipinjam = false; }
+
+    friend class Petugas;
+    friend void lihatStatistik(Admin&, Buku&);
+};
+
 
